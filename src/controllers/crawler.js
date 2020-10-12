21547a0b74df75a71b0011876e52b6f9b2b1b2a0
Eduardo Dias] <carloseduardodiasm@gmail.com>
2020-10-12 14:38:48 -0300

    returning the locals avarege rate.

--- src/controllers/crawler.js
+++ src/controllers/crawler.js
@@ -9,20 +9,51 @@ async function fecthData (url) {
 
 }
 
+function parseStringToFloat (string) {
+  const dottedString = string.replace(',', '.')
+  const decimal = parseFloat(dottedString)
+  return decimal
+}
+
 module.exports = async (req, res) => {
 
   const { name, city } = req.body
-
-  // search params
+  
+  // query params
   const ss = `${name} ${city}`
   const query = querystring.encode({ ss })
-
+  
   const url = `https://www.booking.com/searchresults.pt-br.html?${query}`
-  const html = await fecthData(url) 
+  const responseBody = await fecthData(url) 
+  
+  const $ = cheerio.load(responseBody)
 
-  const $ = cheerio.load(html)
-  console.log($('.sr-hotel__name').text())
+  var hotelsCardsClass = '.sr_item.sr_item_new.sr_item_default.sr_property_block.sr_card_no_hover.sr_item_no_dates'
 
-  return res.json(html)
+  $(hotelsCardsClass).each((index, element) => {
+
+    const localName = $(element).find('span.sr-hotel__name').text()
+    const localScore = $(element).attr('data-score')
+
+    // standardizing the local names
+    let lowerLocalName = localName.toLowerCase()
+    let lowerReqName = name.toLowerCase()
+
+    if (lowerLocalName.includes(lowerReqName)) {
+      foundScore = true
+      score = parseStringToFloat(localScore)
+    }
+
+  })
+
+  if (foundScore) {
+    return res.status(200).json({
+      score: score
+    })
+  }
+
+  return res.status(200).json({
+    score: 0
+  })
 
 }
